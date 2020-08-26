627411419af56c94f985c29379faec923619e74b
tin <tin11011997@gmail.com>
2020-08-26 11:06:48 +0700

    .

---
+++ ToKhaiYTe/ToKhaiYTe/ToKhaiYTe/Models/Reporter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToKhaiYTe.Models
+{
+    public class Reporter
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int phone { get; set; }
+        public int idGender { get; set; }
+        public string psssport { get; set; }
+        public int idCountry { get; set; }
+    }
+}
---
+++ ToKhaiYTe/ToKhaiYTe/ToKhaiYTe/Models/Schedule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToKhaiYTe.Models
+{
+    public class Schedule
+    {
+        public int id { get; set; }
+        public int idVehicle { get; set; }
+        public string vehicleNumber { get; set; }
+        public string seat { get; set; }
+        public DateTime departureDate { get; set; }
+        public DateTime entryDate { get; set; }
+        public int idCountryFrom { get; set; }
+        public int idCityFrom { get; set; }
+        public int idToCountry { get; set; }
+        public int idToCity { get; set; }
+        public string note { get; set; }
+        public int idUser { get; set; }
+
+    }
+}
